e4490bfc4a81c6a4546952dd17932091f6efa286
Scott Bronson <brons_git@u32.net>
2008-04-22 13:33:43 -0700

    Generate Hobo Front: /script/generate hobo_front_controller front --add-routes --delete-index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
 
+  map.site_search  'search', :controller => 'front', :action => 'search'
+  map.homepage '', :controller => 'front', :action => 'index'
+
   Hobo.add_routes(map)
 
   # The priority is based upon order of creation: first created -> highest priority.
